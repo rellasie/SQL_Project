27798902fc34b26a20f093bf26390b7bb5452fdd
Halcolt <billtrinh19@gmail.com>
2022-06-26 12:19:15 +0700

    suggetion file

---
+++ insert_suggestion.sql
@@ -0,0 +1,8 @@
+use weight_tracker
+insert into Suggestions (Tier, Message)
+values
+('1','You are now under weight. You should eat more to keep yourself healthy'),
+('2','Your body is balance but should should eat a bit more a day to keep your weight'),
+('3','Your body is currently balance, keep your diet steady as it is!'),
+('4','Your body is currently overweight but you getting slimer, keep going!'),
+('5','You are overweight, you should eat less and exercise more to be healthy')
