15583cb4c0a27f07dcdbc6d0b28e9e467ec61395
Halcolt <billtrinh19@gmail.com>
2022-07-23 13:22:46 +0700

    new

--- _init_.sql
+++ _init_.sql
@@ -1,6 +1,8 @@
 create database weight_tracker /*Run this first*/
 use weight_tracker /*After that, run this*/
 
+drop database weight_tracker 
+
 /*Then run all lines below to create tables*/
 
 create table Users(
@@ -9,6 +11,80 @@ create table Users(
 	email varchar(255) not null,
 	passwrd varchar(255) not null,
 	constraint keyuser primary key (UserID)
+);
+
+create table Daily_record(
+	[UserID] int,
+	[Date] date,
+	[Cal_in] int,
+	[Cal_burn] int,
+	[Calories_balance] int, /* Calo_balance = Sum(Intake) - Sum(Outtake)*/
+	constraint keyrecord primary key ([UserID], [Date]),
+	constraint DailyData foreign key ([UserID]) references Users([UserID]) 
+);
+
+
+create table Food_list(
+	[FoodID] int identity(1,1),
+	[Food_name] varchar(255) not null unique,
+	[Calories] int not null, /* The average amount of taken calories for each 100g*/
+	constraint keyfood primary key ([FoodID])
+);
+
+create table Menu(
+	[MenuID] int,
+	[FoodID] int not null,
+	[Amount] int not null, 
+	[Intake] int, /* Intake calories = Food(Calories) * Amount*/
+	constraint PK_Menu primary key ([MenuID]),
+	constraint FK_Menu foreign key ([FoodID]) references Food_list([FoodID])
+);
+
+create table Daily_Menu(
+	[UserID] int,
+	[Date] date,
+	[MenuID] int,
+	[cal_in] int,
+	constraint Daily_menu_record primary key ([UserID],[Date],[MenuID]),
+	constraint User_date_m foreign key ([UserID],[Date]) references Daily_record([UserID],[Date]),
+	constraint Example_menu foreign key ([MenuID]) references Menu([MenuID])
+);
+
+
+
+create table Activity_list(
+	[ActivityID] int identity(1,1),
+	[Activity_name] varchar(255) not null unique,
+	[Calories] int not null, /* The average amount of burnt calories for each min*/
+	constraint keyact primary key ([ActivityID])
+);
+
+
+
+
+create table Workout(
+	[ExerciseID] int,
+	[ActivityID] int not null,
+	[Duration] int not null, /* time for each activity*/
+	[Outtake] int, /* Outtake = Activity * duration */
+	constraint PK_Workout primary key ([ExerciseID]),
+	constraint FK_Workout foreign key ([ActivityID]) references  Activity_list([ActivityID])
+);
+
+create table Daily_Workout(
+	[UserID] int,
+	[Date] date,
+	[ExerciseID] int,
+	[cal_burn] int,
+	constraint Daily_workout_record primary key ([UserID],[Date],[ExerciseID]),
+	constraint User_date_w foreign key ([UserID],[Date]) references Daily_record([UserID],[Date]),
+	constraint Example_Exercise foreign key ([ExerciseID]) references Workout([ExerciseID])
+);
+
+create table Suggestions(
+	[Tier] int, /* 1 2 3 4 5*/
+	[Message] varchar(255),
+	constraint key_suggest primary key ([Tier])
 );
 
 create table Body_index(
@@ -26,50 +102,8 @@ create table Body_index(
 	[Calories_balance] int, /*Update from the lastest daily record*/
 	[Tier] int, /* 1 - 2 - 3 - 4 - 5, also update from Calories_balance*/
 	constraint keyindex primary key ([UserID]),
-	constraint foreignkey1 foreign key ([UserID]) references Users([UserID])
-);
-
-create table Food_list(
-	[FoodID] int identity(1,1),
-	[Food_name] varchar(255) not null unique,
-	[Calories] int not null, /* The average amount of taken calories for each 100g*/
-	constraint keyfood primary key ([FoodID])
+	constraint foreignkey1 foreign key ([UserID]) references Users([UserID]),
+	constraint foreignkey2 foreign key ([Tier]) references Suggestions([Tier])
 );
 
 
-create table Menu(
-	[MenuID] int not null,
-	[FoodID] int not null,
-	[Amount] int not null, 
-	[Intake] int, /* Intake calories = Food(Calories) * Amount*/
-);
-
-
-create table Activity_list(
-	[ActivityID] int identity(1,1),
-	[Activity_name] varchar(255) not null unique,
-	[Calories] int not null, /* The average amount of burnt calories for each min*/
-	constraint keyact primary key ([ActivityID])
-);
-
-create table Workout(
-	[ExerciseID] int not null,
-	[ActivityID] int not null,
-	[Duration] int not null, /* time for each activity*/
-	[Outtake] int, /* Outtake = Activity * duration */
-);
-
-create table Daily_record(
-	[UserID] int,
-	[Date] date not null,
-	[MenuID] int not null, /*Can choose from menu list, or create a new one*/
-	[ExerciseID] int not null, /*Can choose from workout list, or create a new one*/
-	[Calories_balance] int, /* Calo_balance = Sum(Intake) - Sum(Outtake)*/
-	constraint keyrecord primary key ([UserID], [Date])
-);
-
-create table Suggestions(
-	[Tier] int, /* 1 2 3 4 5*/
-	[Message] varchar(255),
-	constraint key_suggest primary key ([Tier])
-);
